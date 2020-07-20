f869bf5d81fee9c59b936668c8db0b16cd9dcd95
carolinemao <carolinemao37@gmail.com>
2020-07-20 06:12:34 -0400

    Added about stuff

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-shfhdh
+function loading() {
+document.body.style.opacity = "1";
+// document.body.style.background="green";
+document.body.style.transitionDuration = "2s";
+
+document.getElementById("purple_diagonal").style.width="490px";
+document.getElementById("purple_diagonal").style.transitionDuration = "2s";
+document.getElementById("purple_diagonal").style.transform = "rotate(-43.86deg)";
+}
