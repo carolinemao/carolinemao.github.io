1244e7b1e814b23337f150a74206ab6d4ed36732
carolinemao <carolinemao37@gmail.com>
2020-08-20 16:58:02 -0400

    added projects to homepage

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 function loading() {
 document.body.style.opacity = "1";
-// document.body.style.background="green";
 document.body.style.transitionDuration = "2s";
 
 document.getElementById("purple_diagonal").style.width="490px";
 document.getElementById("purple_diagonal").style.transitionDuration = "2s";
 document.getElementById("purple_diagonal").style.transform = "rotate(-43.86deg)";
 }
-
-// var bodyHeight = document.querySelector("body").offsetHeight;
-// var footer = document.getElementById("contact");
-// contact.style.cssText = "margin-top:"
