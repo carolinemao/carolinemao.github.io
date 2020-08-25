fbc543b85f47b69f5b7ab1ec415e563a43968911
carolinemao <carolinemao37@gmail.com>
2020-08-25 12:21:32 -0400

    more mobile edits - started menu

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+var mobile = window.matchMedia( "(max-width: 550px)" );
+var purple = document.getElementById("purple_diagonal");
 function loading() {
+if (mobile.matches) {
 document.body.style.opacity = "1";
 document.body.style.transitionDuration = "2s";
+purple.style.width="200px";
+purple.style.transitionDuration = "1s";
+purple.style.transform = "rotate(-43.86deg)";
+} else {
+document.body.style.opacity = "1";
+document.body.style.transitionDuration = "2s";
+purple.style.width="490px";
+purple.style.transitionDuration = "2s";
+purple.style.transform = "rotate(-43.86deg)";
+}
+}
 
-document.getElementById("purple_diagonal").style.width="490px";
-document.getElementById("purple_diagonal").style.transitionDuration = "2s";
-document.getElementById("purple_diagonal").style.transform = "rotate(-43.86deg)";
+var menu = document.getElementById("menu_links");
+function showMenu() {
+menu.style.display = "flex";
+menu.style.cssText = "flex-direction:column; justify-content:center;";
 }
