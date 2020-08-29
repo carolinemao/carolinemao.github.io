542b33ed39c9d4b71837b08a81b37b294022e708
carolinemao <carolinemao37@gmail.com>
2020-08-29 10:46:57 -0400

    added more cvc images, edited homepage effects

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ purple.style.transform = "rotate(-43.86deg)";
 } else {
 document.body.style.opacity = "1";
 document.body.style.transitionDuration = "2s";
-purple.style.width="490px";
+purple.style.width="390px";
+// purple.style.height="330px";
 purple.style.transitionDuration = "2s";
 purple.style.transform = "rotate(-43.86deg)";
 }
@@ -20,4 +21,9 @@ var menu = document.getElementById("menu_links");
 function showMenu() {
 menu.style.display = "flex";
 menu.style.cssText = "flex-direction:column; justify-content:center;";
+}
+
+var design = document.getElementById("design");
+function showDetail() {
+design.style.display = "block";
 }
